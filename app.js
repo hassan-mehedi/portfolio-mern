4f85c0528037283c70bac184bebbb3ae14eb6263
hassan-mehedi <howlader.mehedihassan@gmail.com>
2022-03-11 22:29:37 +0600

    feat: add blog controller and route

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require("express");
-const connectDB = require("./config/db");
-const userRoute = require("./routes/api/user.routes");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+const config = require("./config/config");
 
-const PORT = process.env.PORT || 5000;
+const blogRoute = require("./routes/api/blog.routes");
 
-connectDB();
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+// connect to the database
+mongoose
+    .connect(config.mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log(`Database connected successfully`))
+    .catch((err) => console.log(err));
+
+// Start the server
+app.listen(config.PORT, () => console.log(`Listening on ${config.PORT}`));
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api/users", userRoute);
+app.use("/api/blogs", blogRoute);
